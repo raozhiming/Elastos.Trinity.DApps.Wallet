de4e7ae22090e47752f1ade8f8bfcf8308ad84fe
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-01-09 09:44:39 +0800

    Publish script update

--- store/publish.sh
+++ store/publish.sh
@@ -1,1 +1,1 @@
-trinity-cli publish --did did:elastos:iodCjExqz9vNR5mP4uL2rT3VkGKCsMpoKv#primary --didstore ../../../../DAppSignatures/org.elastos.trinity.dapp.wallet/appdid/
+trinity-cli publish --news "$1" --did did:elastos:iodCjExqz9vNR5mP4uL2rT3VkGKCsMpoKv#primary --didstore ../../../../DAppSignatures/org.elastos.trinity.dapp.wallet/appdid/
