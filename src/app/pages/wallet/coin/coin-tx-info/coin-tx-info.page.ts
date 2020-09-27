7b8114f690c470f44a9e57a1f3f4f8bbd1ca03b3
raozhiming <raozhiming@elastos.org>
2020-09-28 00:24:19 +0800

    CU-4hna4z the amount don't subtract fee if fee is too small

--- src/app/pages/wallet/coin/coin-tx-info/coin-tx-info.page.ts
+++ src/app/pages/wallet/coin/coin-tx-info/coin-tx-info.page.ts
@@ -40,7 +40,6 @@ export class CoinTxInfoPage implements OnInit {
     public direction: string = '';
     public symbol: string = '';
     public amount: BigNumber;
-    public displayAmount = '';
     public status: string = '';
 
     // Other Values
@@ -94,7 +93,6 @@ export class CoinTxInfoPage implements OnInit {
             // Header display values
             this.type = this.transactionInfo.type;
             this.amount = this.transactionInfo.amount;
-            this.displayAmount = this.amount.toFixed(6);
             this.symbol = this.transactionInfo.symbol;
             this.payStatusIcon = this.transactionInfo.payStatusIcon;
             const direction = this.transactionInfo.direction;
@@ -124,8 +122,10 @@ export class CoinTxInfoPage implements OnInit {
         // Get tx fees, final tx amount and receiving address
         if ((this.chainId === StandardCoinName.ELA) || (this.chainId === StandardCoinName.IDChain)) { // ELA, IDChain
             this.payFee = transaction.Fee / Config.WEI;
-            this.amount = this.amount.minus(this.payFee);
-            this.displayAmount = this.amount.toFixed(6);
+            // If the fee is too small, then amount doesn't subtract fee
+            if (transaction.Fee > 10000000000) {
+              this.amount = this.amount.minus(this.payFee);
+            }
             this.targetAddress = this.getTargetAddressFromTransaction(transaction);
         } else {
             // TODO: How to distinguish between ordinary transfers and smart contracts
